b1d0eb93cbe69802840277fe0370c9f4de4d8fe8
KriniX <freakgaming2009@gmail.com>
2025-10-18 16:26:48 +0530

    Update assets.sh

--- assets.sh
+++ assets.sh
@@ -10,3 +10,10 @@ yarn # Installs panel build dependencies
 cd /var/www/pterodactyl
 export NODE_OPTIONS=--openssl-legacy-provider # for NodeJS v17+
 yarn build:production # Build panel
+
+yarn remove react-icons
+# OR if you use npm:
+# npm uninstall react-icons
+yarn add react-icons@5.4.0
+# OR if you use npm:
+# npm install react-icons@5.4.0
